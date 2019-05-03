90efb8f4af9eddbb5cadbdd45df2e242c1c874f6
Elizeu Castro <elizeuvoraf@gmail.com>
2019-05-02 21:46:40 -0400

    migrate

---
+++ src/app/models/User.js
@@ -0,0 +1,22 @@
+const bcrypt = require('bcryptjs')
+
+module.exports = (Sequelize, DataTypes) => {
+  const User = Sequelize.define('User', {
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    avatar: DataTypes.STRING,
+    password: DataTypes.VIRTUAL,
+    password_hash: DataTypes.STRING,
+    provider: DataTypes.BOOLEAN
+  }, {
+    hooks: {
+      beforeSave: async user => {
+        if (user.password) {
+          user.password_hash = await bcrypt.hash(user.password, 8)
+        }
+      }
+    }
+  })
+
+  return User
+}
--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,8 @@
 module.exports = {
   dialect: 'postgres',
   host: '127.0.0.1',
-  username: 'postgres',
-  password: 'postgres',
+  username: 'docker',
+  password: 'docker',
   database: 'gonodemodulo2',
   operatorAliases: false,
   define: {
